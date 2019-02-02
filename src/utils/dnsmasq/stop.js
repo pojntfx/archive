8d19eaca5050e4b01afe13c12ef18d1d44c33a10
Felix Pojtinger <felix@pojtinger.com>
2019-02-02 14:51:26 +0100

    refactor(provisioner): Use pm2 JS instead of Bash API

--- src/utils/dnsmasq/stop.js
+++ src/utils/dnsmasq/stop.js
@@ -1,3 +1,11 @@
-const shell = require("shelljs");
+const pm2 = require("pm2");
 
-module.exports.stop = async () => shell.exec("pm2 stop dnsmasq");
+module.exports.stop = async () =>
+  new Promise(resolve =>
+    pm2.connect(() => {
+      pm2.stop("dnsmasq -k --log-dhcp --log-queries", (_, res) => {
+        pm2.disconnect();
+        resolve(res);
+      });
+    })
+  );
