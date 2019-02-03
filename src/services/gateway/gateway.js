534a9e052118fd2b577d3becc53f81036c687f4c
Felix Pojtinger <felix@pojtinger.com>
2019-02-03 15:53:49 +0100

    refactor(provisioner): Use database for tunnel state

--- src/services/gateway/gateway.js
+++ src/services/gateway/gateway.js
@@ -1,10 +1,34 @@
 const Gateway = require("moleculer-web");
+const DbService = require("moleculer-db");
+const SqlAdapter = require("moleculer-db-adapter-sequelize");
+const Sequelize = require("sequelize");
 const localtunnel = require("localtunnel");
+const { MoleculerError } = require("moleculer").Errors;
 
 module.exports = {
   name: "gateway",
-  mixins: [Gateway],
+  mixins: [Gateway, DbService],
+  adapter: new SqlAdapter(process.env.GATEWAY_DB_URI),
+  model: {
+    name: "localtunnel",
+    define: {
+      domain: Sequelize.STRING,
+      nodeId: Sequelize.STRING
+    }
+  },
   settings: {
+    entityValidator: {
+      domain: "string",
+      nodeId: "string"
+    },
+    cors: {
+      origin: "*",
+      methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+      allowedHeaders: [],
+      exposedHeaders: [],
+      credentials: false,
+      maxAge: 3600
+    },
     routes: [
       {
         whitelist: [
@@ -22,7 +46,8 @@ module.exports = {
           "preseeds.*",
           "postseeds.*",
           "gateway.expose",
-          "gateway.unexpose"
+          "gateway.list",
+          "gateway.delete"
         ],
         aliases: {
           // bootmedia
@@ -39,7 +64,10 @@ module.exports = {
           "GET pxeboot/interfaces": "dnsmasq.interfaces",
           "REST pxeboot/mainscripts": "ipxescripts",
           "REST pxeboot/preseeds": "preseeds",
-          "REST pxeboot/postseeds": "postseeds"
+          "REST pxeboot/postseeds": "postseeds",
+          "POST tunnels": "gateway.expose",
+          "GET tunnels": "gateway.list",
+          "DELETE tunnels/:id": "gateway.delete"
           // Doesn't work?
           // - Have you whitelisted the service above?
           // - Try restarting the container
@@ -53,39 +81,83 @@ module.exports = {
       params: {
         subdomain: "string"
       },
-      handler: ctx => {
+      handler: async function(ctx) {
         const that = this;
-        return new Promise(async function(resolve) {
-          const nodes = await ctx.call("$node.list");
-          const localNode = nodes.find(node => node.local);
-          localtunnel(
-            3000,
+        if (ctx.params.subdomain.includes(".")) {
+          throw new MoleculerError(
+            'The subdomain must not include an invalid character such as "."',
+            422,
+            "ERR_INVALID_SUBDOMAIN"
+          );
+        } else {
+          try {
+            return new Promise(async (resolve, reject) => {
+              const nodes = await ctx.call("$node.list");
+              const localNode = nodes.find(node => node.local);
+              localtunnel(
+                3000,
+                {
+                  subdomain: ctx.params.subdomain
+                },
+                async (err, tunnel) => {
+                  if (err) {
+                    reject(err);
+                  }
+                  that[`TUNNEL_${tunnel.url}`] = tunnel;
+                  const tunnelInDB = await this.actions.create({
+                    domain: tunnel.url,
+                    nodeId: localNode.id
+                  });
+                  resolve(tunnelInDB);
+                }
+              );
+            });
+          } catch (e) {
+            throw new MoleculerError(
+              "The tunnel could not be created",
+              503,
+              "ERR_TUNNEL_CREATION_NOT_WORKING"
+            );
+          }
+        }
+      }
+    },
+    delete: {
+      params: {
+        id: "string"
+      },
+      handler: async function(ctx) {
+        try {
+          const tunnelInDb = await this.actions.remove({
+            id: ctx.params.id
+          });
+          await ctx.call(
+            "gateway.unexpose",
             {
-              subdomain: ctx.params.subdomain
+              domain: tunnelInDb.domain,
+              nodeId: tunnelInDb.nodeId
             },
-            function(_, tunnel) {
-              that[`${tunnel.url}-${localNode.id}`] = tunnel;
-              resolve({
-                domain: tunnel.url,
-                nodeId: localNode.id
-              });
+            {
+              nodeId: tunnelInDb.nodeId
             }
           );
-        });
+          return tunnelInDb;
+        } catch (e) {
+          throw new MoleculerError(
+            "This tunnel could not be found",
+            404,
+            "ERR_TUNNEL_NOT_FOUND"
+          );
+        }
       }
     },
     unexpose: {
       params: {
-        domain: "string",
-        nodeId: "string"
+        domain: "string"
       },
-      handler: async ctx => {
-        const tunnel = this[`${ctx.params.domain}-${ctx.params.nodeId}`];
-        await tunnel.close();
-        return {
-          domain: ctx.params.domain,
-          nodeId: ctx.params.nodeId
-        };
+      handler: async function(ctx) {
+        const tunnel = this[`TUNNEL_${ctx.params.domain}`];
+        return await tunnel.close();
       }
     }
   }
